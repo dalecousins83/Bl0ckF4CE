df0d92d4e09ab25505de2840393635fd11f43443
dalecousins83 <47048091+dalecousins83@users.noreply.github.com>
2025-02-19 20:30:45 +1030

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def format_for_logstash(contract_data, contract_details, contract_creator, creat
     #build log event payload
     log_entry = {
         "timestamp": timestamp,
-        "contract_address": contract_data.get("Address"),
+        "contract_address": contract_data.get("contractAddress"),
         "creator_address": contract_creator,
         "abi": contract_details.get("result"),
         "function_calls": contract_data.get("functionCalls"),  # You can customize this further
