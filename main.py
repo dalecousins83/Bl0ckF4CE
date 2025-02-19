eea53d4de4a8cfb3597acc47d9be444ee02a9f57
dalecousins83 <47048091+dalecousins83@users.noreply.github.com>
2025-02-19 20:35:09 +1030

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,8 @@ def format_for_logstash(contract_data, contract_details, contract_creator, creat
         "timestamp": timestamp,
         #"contract_address": contract_data.get("contractAddress"),
         "contract_address": contract_data['address'],
-        "creator_address": contract_creator,
+        "creator_address": contract_data['creator_address'],
+        "creator_address2": contract_creator,
         "abi": contract_details.get("result"),
         "function_calls": contract_data.get("functionCalls"),  # You can customize this further
         #"risk_score": assess_risk(contract_data, contract_details),
